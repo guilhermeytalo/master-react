3f141a1e5c4118c9f5830d706473506ed9cc837f
Guilherme Ytalo <gydaytalo@gmail.com>
2022-06-25 22:26:40 -0300

    first commit for new branch

--- src/App.js
+++ src/App.js
@@ -6,31 +6,23 @@ class App extends Component {
   constructor() {
     super();
     this.state =  {
-      name: {firstName: 'Guilherme', lastName: 'Lima'},
-      company: 'Google',
+      monster1: {
+        name: 
+        'Linda',
+      },
+      monster2: {
+        name: 'Frank',
+      },
+      monster3: {
+        name: 'Jacky'
+      }
     };
-  }
-
-  onChangeNameState = () => {
-    this.setState({
-      name: {firstName: 'Guilherme', lastName: 'Ytalo'},
-      company: 'ZTM',
-    }, () => { console.log(this.state) });
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.setState.name.lastName}, I work
-            at {this.state.company}
-          </p>
-          <button onClick={this.onChangeNameState}>
-              Change Name
-            </button>
-        </header>
+        
       </div>
     );   
   }
