17646d58271a56d5fda86e8ddbf9ee3c864bcab5
Guilherme Ytalo <gydaytalo@gmail.com>
2022-09-10 23:10:05 -0300

    hust s search box

--- src/App.js
+++ src/App.js
@@ -10,29 +10,34 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((users) => 
-      this.setState(
-        () => {
-        return { monster: users };
-        },
-        () => {
-          console.log(this.setState)
-        }
-      )
-    );
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monster: users };
+          },
+          () => {
+            console.log(this.setState);
+          }
+        )
+      );
   }
 
   render() {
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+        />
         {this.state.monster.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
-          )
+          );
         })}
       </div>
     );
