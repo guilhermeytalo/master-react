e216ff95fd1396c4ee339bfdd064467714ab4380
Guilherme Ytalo <guilherme.ytalo@cumbuca.com.br>
2022-11-02 21:22:32 -0300

    created a filtered list and storigin original data

--- src/App.js
+++ src/App.js
@@ -1,57 +1,65 @@
 import "./App.css";
-import { Component } from "react";
+import {Component} from "react";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monster: [],
+    constructor() {
+        super();
+        this.state = {
+            monster: [],
+            searchField: '',
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((response) => response.json())
+            .then((users) =>
+                this.setState(
+                    () => {
+                        return {monster: users};
+                    },
+                    () => {
+                        console.log(this.setState);
+                    }
+                )
+            );
+    }
+
+    monsterName = (name) => {
+        console.log({startingArray: this.state.monster});
+        const searchField = name.target.value.toLocaleLowerCase();
+
+        this.setState(() => {
+            return {searchField}
+        }, () => {
+            console.log({endingArray: this.state.monster});
+        });
     };
-  }
 
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monster: users };
-          },
-          () => {
-            console.log(this.setState);
-          }
-        )
-      );
-  }
 
-  monsterName = (name) => {
-    const searchValue = name.target.value.toLocaleLowerCase();
-    const filteredMonster = this.state.monster.filter((monster) =>
-      monster.name.toLocaleLowerCase().includes(searchValue)
-    );
+    render() {
+        const filteredMonster = this.state.monster.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+        });
 
-    this.setState(() => {return { monster: filteredMonster }});
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="search monsters"
-          onChange={this.monsterName}
-        />
-        {this.state.monster.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
+        return (
+            <div className="App">
+                <input
+                    className="search-box"
+                    type="search"
+                    placeholder="search monsters"
+                    onChange={this.monsterName}
+                />
+                {filteredMonster.map((monster) => {
+                    return (
+                        <div key={monster.id}>
+                            <h1>{monster.name}</h1>
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
